9ba9260c03da42dde9ff8051f54837ee26197924
frankielb <frankie.brown1610@icloud.com>
2025-02-13 20:10:02 +0000

    3. setup asteroids and collisions

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import pygame
 from constants import SCREEN_HEIGHT,SCREEN_WIDTH,ASTEROID_KINDS,ASTEROID_MAX_RADIUS,ASTEROID_MIN_RADIUS,ASTEROID_SPAWN_RATE
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 def main():
     pygame.init()
     print('Starting asteroids!')
-    print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
+    #print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
     clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
@@ -13,6 +15,12 @@ def main():
     drawable = pygame.sprite.Group()
     Player.containers = (updatable,drawable)
     player = Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
+
+    asteroids = pygame.sprite.Group()
+    Asteroid.containers = (asteroids, updatable, drawable)
+
+    AsteroidField.containers = updatable
+    asteroidfield = AsteroidField()
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -20,6 +28,11 @@ def main():
             
         for u in updatable:
             u.update(dt)
+        
+        for a in asteroids:
+            if a.collision(player):
+                print('Game over!')
+                return
         screen.fill((0,0,0))
         for d in drawable:
             d.draw(screen)
--- player.py
+++ player.py
@@ -14,10 +14,13 @@ class Player(CircleShape):
         b = self.position - forward * self.radius - right
         c = self.position - forward * self.radius + right
         return [a, b, c]
+    
     def draw(self, screen):
         pygame.draw.polygon(screen,'white',self.triangle(),2)
+
     def rotate(self,dt):
         self.rotation += PLAYER_TURN_SPEED * dt
+        
     def update(self, dt):
         keys = pygame.key.get_pressed()
 
